7233b1d6b208d0edce80c87f96109eae237b0b13
diciommoriccardo <riccardo.diciommo1@gmail.com>
2022-01-29 19:10:54 +0100

    changed chart

--- js/demo/chart-bar-demo.js
+++ js/demo/chart-bar-demo.js
@@ -1,6 +1,6 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#858796';
+// Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+// Chart.defaults.global.defaultFontColor = '#858796';
 
 function number_format(number, decimals, dec_point, thousands_sep) {
   // *     example: number_format(1234.56, 2, ',', ' ');
@@ -27,6 +27,107 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   return s.join(dec);
 }
 
+//sezione per acquisizion dati cluster da server
+
+var clusters = []
+let lab = []
+let tot = []
+let colors = []
+
+var randomColorGenerator = function () { 
+  return '#' + (Math.random().toString(16) + '0000000').slice(2, 8); 
+};
+
+$.get("http://151.0.197.14:10000/api/clusters/all", function(data){
+  for (const key in data) {
+    lab.push(data[key]["name"]); //vettore nomi cluster
+    tot.push(data[key]["userId"].length); //vettore tot utenti per cluster
+    colors.push(randomColorGenerator()) // vettore per colori background
+    clusters.push({"name": data[key]["name"], "totalUsers": data[key]["userId"].length, "usersId": data[key]["userId"]}) //mappa cluster
+  }
+
+  var data = {
+    datasets: [{
+      label: "Utenti nel cluster",
+      data: tot,
+      backgroundColor: colors,
+      borderColor: colors,
+    }],
+    labels: lab
+  }
+
+  var ctx = document.getElementById("barChart");
+  var barChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    backgroundColor: "#4e73df",
+    hoverBackgroundColor: "#2e59d9",
+    borderColor: "#4e73df",
+    options: {
+      title: {
+        display: true,
+        text: 'Popolazione clusters'
+      },
+      maintainAspectRatio: false,
+      layout: {
+        padding: {
+          left: 10,
+          right: 25,
+          top: 25,
+          bottom: 0
+        }
+      },
+      scales: {
+        xAxes: [{
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          ticks: {
+            maxTicksLimit: 6
+          },
+          maxBarThickness: 25,
+        }],
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: 15000,
+            maxTicksLimit: 5,
+            padding: 10,
+          },
+          gridLines: {
+            color: "rgb(234, 236, 244)",
+            zeroLineColor: "rgb(234, 236, 244)",
+            drawBorder: false,
+            borderDash: [2],
+            zeroLineBorderDash: [2]
+          }
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        titleMarginBottom: 10,
+        titleFontColor: '#6e707e',
+        titleFontSize: 14,
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        caretPadding: 10,
+      },
+    }
+  }) 
+});
+  console.log(lab)
+  console.log(tot)
+  console.log(clusters)
+
+
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myBarChart = new Chart(ctx, {
@@ -41,71 +142,5 @@ var myBarChart = new Chart(ctx, {
       data: [4215, 5312, 6251, 7841, 9821, 14984],
     }],
   },
-  options: {
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 0
-      }
-    },
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
-        },
-        ticks: {
-          maxTicksLimit: 6
-        },
-        maxBarThickness: 25,
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          callback: function(value, index, values) {
-            return ' ' + number_format(value);
-          }
-        },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
-    legend: {
-      display: false
-    },
-    tooltips: {
-      titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
-      titleFontSize: 14,
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-      callbacks: {
-        label: function(tooltipItem, chart) {
-          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ' ' + number_format(tooltipItem.yLabel);
-        }
-      }
-    },
-  }
+  
 });
